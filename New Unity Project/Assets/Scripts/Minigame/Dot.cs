7c5e4f2f9bd14e97bbd6ca411df2b259f7f84a37
Chamberlain <chc899@usask.ca>
2019-01-26 22:26:58 -0600

    Implemented dot path minigame

---
+++ New Unity Project/Assets/Scripts/Minigame/Dot.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Dot : MonoBehaviour
+{
+    public SpriteRenderer Renderer;
+
+    public bool IsSource;
+    public bool IsTarget;
+
+    public bool IsSelected;
+
+    public Dot[] Neighbors;
+
+    public MinigameInstance Minigame;
+    public Coordinate Coordinate;
+
+    void Start()
+    {
+        GetComponentRefs();
+    }
+
+    void Update()
+    {
+        Renderer.color = GetDotColor(IsSource, IsTarget, IsSelected);
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left Click
+        {
+            if (CanSelect && !(IsSource || IsSelected))
+            {
+                Minigame.DotMarked(this);
+                IsSelected = true;
+            }
+        }
+    }
+
+    private bool CanSelect
+    {
+        get
+        {
+            foreach (var neighbor in Neighbors)
+            {
+                if (neighbor.IsSource || neighbor.IsSelected)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public static Color GetDotColor(bool source, bool target, bool selected)
+    {
+        if (source) { return Color.blue; }
+        else if (target) { return Color.red; }
+        else
+        {
+            if (selected) { return Color.white; }
+            else { return Color.gray; }
+        }
+    }
+
+    void Reset()
+    {
+        GetComponentRefs();
+    }
+
+    void GetComponentRefs()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+    }
+}
--- New Unity Project/Assets/Scripts/Minigame/MinigameInstance.cs
+++ New Unity Project/Assets/Scripts/Minigame/MinigameInstance.cs
@@ -1,14 +1,166 @@
 ﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MinigameInstance : MonoBehaviour
 {
+    [Header("Game References")]
     public GameObject GameManager;
     public GameObject Caller;
 
+    [Header("Minigame Configuration")]
+    public MinigameSolution Solution;
+    public LineRenderer Wire;
+
+    public bool HasSolution { get { return Solution != null && Solution.IsValid; } }
+
+    [Header("Prefabs")]
+    public GameObject DotPrefab;
+
+    private Dot[,] _dots;
+    private List<Dot> _chain;
+
     void Start()
     {
-        GameManager.SendMessage("dimScreen");
-        Invoke("EndMinigame", 5);
+        StartMinigame();
+
+        _chain = new List<Dot>();
+        _dots = new Dot[5, 5];
+
+        // Create dot grid
+        for (var y = 0; y < 5; y++)
+        {
+            for (var x = 0; x < 5; x++)
+            {
+                // Create dot instance
+                var dotGo = Instantiate(DotPrefab, transform);
+                dotGo.transform.position = GetPosition(x, y);
+                dotGo.name = String.Format("dot {0}, {1}", x, y);
+
+                // Configure dot
+                var dot = dotGo.GetComponent<Dot>();
+                dot.Coordinate = new Coordinate(x, y);
+                dot.Minigame = this;
+
+                // 
+                _dots[x, y] = dot;
+            }
+        }
+
+        // Connect dot grid
+        for (var y = 0; y < 5; y++)
+        {
+            for (var x = 0; x < 5; x++)
+            {
+                var dot = GetDot(x, y);
+                dot.Neighbors = GetNeighborDots(x, y).ToArray();
+            }
+        }
+
+        // Augment dots to have solution information
+        if (HasSolution)
+        {
+            // 
+            for (var i = 0; i < Solution.Coordinates.Length; i++)
+            {
+                var dot = GetDot(Solution.Coordinates[i]);
+                dot.IsSource = i == 0;
+                dot.IsTarget = i == Solution.Coordinates.Length - 1;
+                dot.IsSelected = false;
+            }
+        }
+
+        // Automatically contains the source
+        _chain.Add(GetDot(Solution.Coordinates[0]));
+        UpdateWire();
+    }
+
+    public void DotMarked(Dot dot)
+    {
+        _chain.Add(dot);
+        UpdateWire();
+
+        // Target clicked, check solution
+        if (dot.IsTarget)
+        {
+            // TODO: Validate Path
+            var success = CheckPathIsCorrect();
+            EndMinigame(success);
+        }
+    }
+
+    private bool CheckPathIsCorrect()
+    {
+        for (var i = 0; i < _chain.Count; i++)
+        {
+            var co1 = Solution.Coordinates[i];
+            var co2 = _chain[i].Coordinate;
+            if (co1 != co2) { return false; }
+        }
+
+        return true;
+    }
+
+    private void UpdateWire()
+    {
+        Wire.positionCount = _chain.Count;
+        for (var i = 0; i < _chain.Count; i++)
+        {
+            var dot = _chain[i];
+            Wire.SetPosition(i, dot.transform.position);
+        }
+    }
+
+    public Vector2 GetPosition(int x, int y)
+    {
+        var xx = ((x / 4F) * 2 - 1) * 12;
+        var yy = ((y / 4F) * 2 - 1) * 12;
+
+        return new Vector2(xx, yy);
+    }
+
+    public Vector2 GetPosition(Coordinate coord)
+    {
+        return GetPosition(coord.X, coord.Y);
+    }
+
+    public bool IsValidDotCoord(int x, int y)
+    {
+        if (x < 0 || x >= 5) { return false; }
+        if (y < 0 || y >= 5) { return false; }
+        return true;
+    }
+
+    public Dot GetDot(int x, int y)
+    {
+        if (!IsValidDotCoord(x, y))
+        {
+            Debug.LogErrorFormat("Invalid dot coordinate {0} {1}", x, y);
+        }
+        return _dots[x, y];
+    }
+
+    public Dot GetDot(Coordinate coord)
+    {
+        return GetDot(coord.X, coord.Y);
+    }
+
+    public IEnumerable<Dot> GetNeighborDots(int x, int y)
+    {
+        for (var offsetY = -1; offsetY <= 1; offsetY++)
+        {
+            for (var offsetX = -1; offsetX <= 1; offsetX++)
+            {
+                // Skip center
+                if (offsetX == 0 && offsetY == 0) { continue; }
+                // If coordinate is valid, return dot
+                else if (IsValidDotCoord(x + offsetX, y + offsetY))
+                {
+                    yield return GetDot(x + offsetX, y + offsetY);
+                }
+            }
+        }
     }
 
     void Update()
@@ -16,15 +168,61 @@ public class MinigameInstance : MonoBehaviour
 
     }
 
+    void StartMinigame()
+    {
+        if (GameManager != null) { GameManager.SendMessage("dimScreen"); }
+    }
+
     void EndMinigame(bool success)
     {
-        GameManager.SendMessage("unDimScreen");
-        if (success) { Caller.SendMessage("success"); }
+        if (GameManager != null) { GameManager.SendMessage("unDimScreen"); }
+        else { Debug.LogWarningFormat("MINIGAME ENDED (success: {0})", success); }
+        if (success && Caller != null) { Caller.SendMessage("success"); }
         Destroy(gameObject);
     }
 
     void exit()
     {
         EndMinigame(false);
+    }
+
+    void OnDrawGizmos()
+    {
+        // 
+        for (var y = 0; y < 5; y++)
+        {
+            for (var x = 0; x < 5; x++)
+            {
+                var color = Color.black;
+
+                // Has a solution set
+                if (HasSolution)
+                {
+                    var idx = Array.FindIndex(Solution.Coordinates, q => q.X == x && q.Y == y);
+                    if (idx >= 0)
+                    {
+                        if (idx == 0) { color = Color.blue; }
+                        else if (idx == Solution.Coordinates.Length - 1) { color = Color.red; }
+                        else { color = Color.white; }
+                    }
+                }
+
+                Gizmos.color = color;
+                Gizmos.DrawWireSphere(GetPosition(x, y), 1);
+            }
+        }
+
+        // 
+        if (HasSolution)
+        {
+            for (var i = 1; i < Solution.Coordinates.Length; i++)
+            {
+                var source = GetPosition(Solution.Coordinates[i - 1]);
+                var target = GetPosition(Solution.Coordinates[i - 0]);
+
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawLine(source, target);
+            }
+        }
     }
 }
