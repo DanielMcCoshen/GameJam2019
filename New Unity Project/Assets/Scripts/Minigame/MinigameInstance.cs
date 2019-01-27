ecc30aaf09832fab84d8b0a1c5b3892f3d2fbc2d
Chamberlain <chc899@usask.ca>
2019-01-26 18:42:45 -0600

    Added hollow minigame objects

---
+++ New Unity Project/Assets/Scripts/Minigame/MinigameInstance.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MinigameInstance : MonoBehaviour
+{
+    public GameObject GameManager;
+    public GameObject Caller;
+
+    void Start()
+    {
+        GameManager.SendMessage("dimScreen");
+        Invoke("EndMinigame", 5);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void EndMinigame(bool success)
+    {
+        GameManager.SendMessage("unDimScreen");
+        if (success) { Caller.SendMessage("success"); }
+        Destroy(gameObject);
+    }
+
+    void exit()
+    {
+        EndMinigame(false);
+    }
+}
