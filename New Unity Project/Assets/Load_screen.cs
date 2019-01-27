b8b9b94a26c599f22be1958d926309738d98fd94
Kreisz <apk436@usask.ca>
2019-01-27 17:09:18 -0600

    menu

---
+++ New Unity Project/Assets/Load_screen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Load_screen : MonoBehaviour {
+
+	public void LoadByIndex()
+	{
+		SceneManager.LoadScene ("Main Game");
+	}
+}
